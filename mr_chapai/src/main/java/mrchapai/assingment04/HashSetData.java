f3d6694411b634715b2b2ff920ff7f8c8960bb0f
mr chapai <myicskt@gmail.com>
2023-07-26 19:31:29 -0500

    "first commit"

---
+++ mr_chapai/src/main/java/mrchapai/assingment04/HashSetData.java
@@ -0,0 +1,14 @@
+package mrchapai.assingment04;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class HashSetData {
+    public static void main(String[] args) {
+        Set<String> mySet =new HashSet<String>();
+        try {
+
+        }
+
+    }
+}
